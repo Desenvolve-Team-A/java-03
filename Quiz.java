87d370324f421d39a05e699a21f8cc74f798983f
Julia E. Mette <juliaemanuele.mette@philips.com>
2024-06-20 14:15:52 -0300

    integração e mudanças

--- Quiz.java
+++ Quiz.java
@@ -1,23 +1,71 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class Quiz {
+  private List<Question> questions;
+  private int score;
+  private int correct;
+  private Interface ui;
 
+  public Quiz(List<Question> questions, Interface ui) {
+    this.questions = new ArrayList<>(questions);
+    this.score = 0;
+    this.correct = 0;
+    this.ui = ui;
+  }
 
+  public void iniciar() {
+    Scanner scanner = new Scanner(System.in);
+    Collections.shuffle(questions);
 
-    public void começarQuiz() {
-        int totalPontos = 0;
-        int respostasCorretas = 0;
+    int numberQuestions = Math.min(questions.size(), 5);
+    List<Question> selectedQuestions = questions.subList(0, numberQuestions);
 
-        //loop para iterar sobre as perguntas selecionadas e as suas opções
+    for (int i = 0; i < selectedQuestions.size(); i++) {
+      Question ask = selectedQuestions.get(i);
+      ui.lineQuestion();
+      System.out.println((i + 1) + "- " + ask.getQuestion() + " Pontuacao: " + score);
 
+      String[] answers = ask.getAnswer();
+      for (int j = 0; j < answers.length; j++) {
+        System.out.println((j + 1) + ": " + answers[j]);
+      }
 
-        //condicional para pegar o peso das questões e somar
-        if (resposta == respostaCerta) {
-            int pontos = perguntaAtual.peso;
-            totalPontos += pontos;
-            respostasCorretas++;
-            System.out.println("Acertou! Você ganhou " + pontos + " pontos.\n");
-        } else {
-            System.out.println("Você errou!.\n");
+      int thisAnswer = -1;
+      boolean checkAnswer = false;
+
+      while (!checkAnswer) {
+        System.out.print("Sua resposta (1-4): ");
+        try {
+          thisAnswer = scanner.nextInt() - 1;
+          if (thisAnswer < 0 || thisAnswer > 3) {
+            System.out.println("Resposta inválida. Por favor, digite um número entre 1 e 4.");
+          } else {
+            checkAnswer = true;
+          }
+        } catch (InputMismatchException e) {
+          System.out.println("Entrada inválida. Por favor, digite um número entre 1 e 4.");
+          scanner.next();
         }
-        scanner.close();
+      }
+
+      if (ask.checkAnswer(thisAnswer)) {
+        System.out.println("Acertou! Você ganhou " + score + " pontos.");
+        correct++;
+        score += (i + 1);
+      } else {
+        System.out.println("Você errou!");
+      }
     }
+
+    ui.printDot();
+    System.out.println("|| Você acertou " + correct + " perguntas.");
+    System.out.println("|| Sua pontuação final é de: " + score);
+    ui.printDot();
+    ui.printSpace();
+
+  }
 }
