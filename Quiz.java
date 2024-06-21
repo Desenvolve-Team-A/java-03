519eaffccf4904d01b2323039dbfd46a23aff5fb
Gabriel Moraes <gabriel.moraes@philips.com>
2024-06-21 14:57:41 -0300

    tirando acentos e adicionando quebra de linha

--- Quiz.java
+++ Quiz.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.InputMismatchException;
 
 public class Quiz {
   private List<Question> questions;
@@ -27,43 +26,43 @@ public class Quiz {
     for (int i = 0; i < selectedQuestions.size(); i++) {
       Question ask = selectedQuestions.get(i);
       ui.lineQuestion();
-      System.out.println((i + 1) + "- " + ask.getQuestion() + " Pontuacao: " + score);
+      System.out.println((i + 1) + "- " + ask.getQuestion() + " Pontuacao: " + score + "\n");
 
       String[] answers = ask.getAnswer();
       for (int j = 0; j < answers.length; j++) {
-        System.out.println((j + 1) + ": " + answers[j]);
+        System.out.println((j + 1) + ") " + answers[j]);
       }
 
       int thisAnswer = -1;
       boolean checkAnswer = false;
 
       while (!checkAnswer) {
-        System.out.print("Sua resposta (1-4): ");
+        System.out.print("\nSua resposta (1-4): ");
         if (scanner.hasNextInt()) {
           thisAnswer = scanner.nextInt() - 1; // Menos 1 porque o array começa em 0;
           if (thisAnswer < 0 || thisAnswer > 3) {
-            System.out.println("Resposta invalida. Por favor, digite um numero entre 1 e 4.");
+            System.out.println("\nResposta invalida. Por favor, digite um numero entre 1 e 4.");
           } else {
             checkAnswer = true;
           }
         } else {
-          System.out.println("Resposta invalida. Por favor, digite um numero entre 1 e 4.");
+          System.out.println("\nResposta invalida. Por favor, digite um numero entre 1 e 4.");
           scanner.next();
         }
       }
 
       if (ask.checkAnswer(thisAnswer)) {
-        System.out.println("Acertou!");
+        System.out.println("\nAcertou!");
         correct++;
         score += (i + 1);
       } else {
-        System.out.println("Você errou!");
+        System.out.println("\nVoce errou!");
       }
     }
 
     ui.printDot();
-    System.out.println("|| Voce acertou " + correct + " perguntas.");
-    System.out.println("|| Sua pontuacao final é de: " + score);
+    System.out.println("\n\n|| Voce acertou " + correct + " perguntas.");
+    System.out.println("|| Sua pontuacao final e de: " + score);
     ui.printDot();
     ui.printSpace();
 
